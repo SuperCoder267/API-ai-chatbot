c743dc48cdb06fde2dd83798faee22488572cd2a
SuperCoder267 <ahmadfarooq710@gmail.com>
2025-03-26 10:40:17 -0400

    Update generateResponse.js

--- api/generateResponse.js
+++ api/generateResponse.js
@@ -1,9 +1,20 @@
 // Serverless handler function
-export default async function handler(req, res) {
+export default async function handler(req, res) { 
     // Add CORS headers
     res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Origin', '*');  
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+
+    // Allowed requests from site
+    const allowedOrigins = [ 
+        'http://127.0.0.1:5503',
+        'http://localhost:5503'
+    ];
+
+    const origin = req.headers.origin;
+        if (allowedOrigins.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+        }
+  
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
 
     // Only allow POST method
